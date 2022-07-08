65ffe8d1192139312513f8388a7213b0717aaeed
unknown <shofondo@gmail.com>
2022-07-07 22:10:27 -0500

    hamburger menu almost done

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".hamburger-menu-container");
+const menu = document.querySelector(".mobile-menu");
+
+button.addEventListener('click', function() {
+    menu.style.display = "block"
+})
