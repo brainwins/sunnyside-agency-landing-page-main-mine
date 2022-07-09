5f55ef93d5f964ec31f418157ae6e3ac95456b64
unknown <shofondo@gmail.com>
2022-07-08 22:49:52 -0500

    mobile done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 const button = document.querySelector(".hamburger-menu-container");
 const menu = document.querySelector(".mobile-menu");
+menu.style.display = "none"
 
 button.addEventListener('click', function() {
-    menu.style.display = "block"
+    if(menu.style.display == "none") {
+        
+        console.log(menu.style.display)
+    menu.style.display = "flex"
+    }
+    else {
+        console.log("disappear")
+    menu.style.display = "none"
+    }
 })
